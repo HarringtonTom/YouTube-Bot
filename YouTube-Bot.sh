2bd3f4e150f006dffdaebdce1174f588979240d8
Tom Harrington <54778979+HarringtonTom@users.noreply.github.com>
2020-06-01 00:08:50 +0100

    Add files via upload

--- YouTube-Bot.sh
+++ YouTube-Bot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#fixes the issue of YouTube-dl not being found in crontab
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+ 
 #enter your youtube-dl command here, look at example in README
-youtube-dl command 
-
+youtube-dl --playlist-end 2 --reject-title 'Photos' --download-archive '/home/tjh/YouTube-Bot/downloaded.txt' -i -o "/home/tjh/YouTube-Bot/videos/%(title)s" -f bestvideo+bestaudio --merge-output-format mkv --batch-file='/home/tjh/YouTube-Bot/channel_list.txt' 
 
 #change user to your user name
-python3 '/home/user/YouTube-Bot/run.py'
+python3 '/home/tjh/YouTube-Bot/run.py'
