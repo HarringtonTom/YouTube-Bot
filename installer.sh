d579e3e716348e5713fe22cc758c8a923fe89a25
Tom Harrington <54778979+HarringtonTom@users.noreply.github.com>
2020-05-31 20:27:42 +0100

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -2,24 +2,24 @@
 
 echo "Updating Packages..." 
 sudo apt update > /dev/null 2>&1
-sudo apt upgrade > /dev/null 2>&1
+sudo apt upgrade -y > /dev/null 2>&1
 
 echo "Installing Python..."
-sudo apt install python3 > /dev/null 2>&1
-sudo apt install python3-pip > /dev/null 2>&1 
-pip3 install google-api-python-client > /dev/null 2>&1
-pip3 install google-auth-oauthlib > /dev/null 2>&1
-pip3 install google-auth-httplib2 > /dev/null 2>&1
-pip3 install oauth2client > /dev/null 2>&1
+sudo apt install -y python3 > /dev/null 2>&1
+sudo apt install -y python3-pip > /dev/null 2>&1 
+pip3 install -y google-api-python-client > /dev/null 2>&1
+pip3 install -y google-auth-oauthlib > /dev/null 2>&1
+pip3 install -y google-auth-httplib2 > /dev/null 2>&1
+pip3 install -y oauth2client > /dev/null 2>&1
 
 echo "Installing YouTube-dl..."
-sudo pip3 install youtube_dl > /dev/null 2>&1
+sudo pip3 install -y youtube_dl > /dev/null 2>&1
 
 echo "Installing ffmpeg..." 
-sudo apt install ffmpeg > /dev/null 2>&1
+sudo apt install -y ffmpeg > /dev/null 2>&1
 
 echo "Installing git..." 
-sudo apt install git > /dev/null 2>&1
+sudo apt install -y git > /dev/null 2>&1
 
 echo -e "\e[31mInstallation Complete" 
 
