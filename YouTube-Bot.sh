2d2d2a7d4d74a2408e1b85ababd9b792c889516d
Tom Harrington <54778979+HarringtonTom@users.noreply.github.com>
2020-05-31 21:18:26 +0100

    Update YouTube-Bot.sh

--- YouTube-Bot.sh
+++ YouTube-Bot.sh
@@ -3,4 +3,6 @@
 #enter your youtube-dl command here, look at example in README
 youtube-dl command 
 
-python3 '/home/user/YouTube-Bot/upload.py'
+
+#change user to your user name
+python3 '/home/user/YouTube-Bot/run.py'
